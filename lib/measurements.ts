33620109bb58f7e09acea8a6eb3763868952aaa5
Hailite <bisaillonpatrick1979@gmail.com>
2026-06-27 17:47:25 -0600

    Add measurement helpers

---
+++ lib/measurements.ts
@@ -0,0 +1,4 @@
+export const feetToMeters = (feet: number) => feet * 0.3048;
+export const inchesToFeet = (inches: number) => inches / 12;
+export const sqftToSqm = (value: number) => value * 0.09290304;
+export const sqmToSqft = (value: number) => value * 10.7639104167;

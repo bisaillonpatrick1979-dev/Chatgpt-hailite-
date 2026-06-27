fa34d043b8e7d065afa6547315124699061ab7c9
Hailite <bisaillonpatrick1979@gmail.com>
2026-06-27 17:49:38 -0600

    Add regional defaults

---
+++ lib/regions.ts
@@ -0,0 +1,12 @@
+export const albertaDefaults = {
+  country: "Canada",
+  region: "Alberta",
+  currency: "CAD",
+  taxName: "GST",
+  taxRate: 5,
+  unitSystem: "imperial",
+  lengthUnit: "ft_in",
+  areaUnit: "sqft"
+};
+
+export const supportedLanguages = ["fr", "en", "es"] as const;

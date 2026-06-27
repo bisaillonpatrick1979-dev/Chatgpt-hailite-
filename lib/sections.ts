2a54340dc1afc43a3fd019f7f2787a3959518b3e
Hailite <bisaillonpatrick1979@gmail.com>
2026-06-27 17:50:03 -0600

    Add default construction sections and tasks

---
+++ lib/sections.ts
@@ -0,0 +1,27 @@
+export const defaultProjectSections = [
+  "front",
+  "back",
+  "left_wall",
+  "right_wall",
+  "north_wall",
+  "south_wall",
+  "east_wall",
+  "west_wall",
+  "garage",
+  "soffit",
+  "fascia",
+  "siding",
+  "trim",
+  "corners"
+];
+
+export const sidingChecklist = [
+  "remove_old_siding",
+  "install_house_wrap",
+  "install_starter",
+  "install_channels",
+  "install_siding",
+  "install_trim",
+  "final_photos",
+  "cleanup"
+];

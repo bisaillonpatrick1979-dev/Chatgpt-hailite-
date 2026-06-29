2a9c600756986c8fd80029e6d190d22955cc126b
Hailite <bisaillonpatrick1979@gmail.com>
2026-06-29 16:03:54 -0600

    Add live test console to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import { AppShell } from "@/components/AppShell";
 import { ExecutiveVisualDashboard } from "@/components/ExecutiveVisualDashboard";
+import { LiveMvpConsole } from "@/components/LiveMvpConsole";
 import { OperationsHub } from "@/components/OperationsHub";
 
 export default function Page() {
   return (
     <AppShell>
       <ExecutiveVisualDashboard />
+      <LiveMvpConsole />
       <OperationsHub />
     </AppShell>
   );

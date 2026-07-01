defbe1af3a4a8a68d962eaa1ac7b758ddac635af
Hailite <bisaillonpatrick1979@gmail.com>
2026-06-30 20:33:45 -0600

    Add access notice to worker portal

--- app/worker/page.tsx
+++ app/worker/page.tsx
@@ -1,9 +1,11 @@
 import { AppShell } from "@/components/AppShell";
 import { PersonalPortalView } from "@/components/PersonalPortalView";
+import { RoleAccessNotice } from "@/components/RoleAccessNotice";
 
 export default function Worker() {
   return (
     <AppShell>
+      <RoleAccessNotice allowedRoles={["Employe"]} />
       <PersonalPortalView type="worker" />
     </AppShell>
   );

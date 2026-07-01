236200f55fa5d8dc7a508ff2c42886920243370f
Hailite <bisaillonpatrick1979@gmail.com>
2026-06-30 20:47:08 -0600

    Block dashboard content by admin role

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,14 +3,17 @@ import { BusinessManagerLive } from "@/components/BusinessManagerLive";
 import { ExecutiveVisualDashboard } from "@/components/ExecutiveVisualDashboard";
 import { LiveMvpConsole } from "@/components/LiveMvpConsole";
 import { OperationsHub } from "@/components/OperationsHub";
+import { RoleGate } from "@/components/RoleGate";
 
 export default function Page() {
   return (
     <AppShell>
-      <ExecutiveVisualDashboard />
-      <BusinessManagerLive />
-      <LiveMvpConsole />
-      <OperationsHub />
+      <RoleGate allowedRoles={["Administrateur"]}>
+        <ExecutiveVisualDashboard />
+        <BusinessManagerLive />
+        <LiveMvpConsole />
+        <OperationsHub />
+      </RoleGate>
     </AppShell>
   );
 }

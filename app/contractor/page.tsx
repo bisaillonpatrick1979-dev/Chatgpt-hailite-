bbb9c49eed60abb566935f8f5b53c6fe17bf1480
Hailite <bisaillonpatrick1979@gmail.com>
2026-06-30 20:34:05 -0600

    Add access notice to contractor portal

--- app/contractor/page.tsx
+++ app/contractor/page.tsx
@@ -1,9 +1,11 @@
 import { AppShell } from "@/components/AppShell";
 import { PersonalPortalView } from "@/components/PersonalPortalView";
+import { RoleAccessNotice } from "@/components/RoleAccessNotice";
 
 export default function Contractor() {
   return (
     <AppShell>
+      <RoleAccessNotice allowedRoles={["Sous-traitant"]} />
       <PersonalPortalView type="contractor" />
     </AppShell>
   );
